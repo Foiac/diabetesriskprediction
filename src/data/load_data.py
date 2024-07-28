ce3307e78bfe495818b7a67d9b22c1250e6a01de
Foiac <kaio_cfs@hotmail.com>
2024-07-27 22:34:37 -0300

    comments e doc string update

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
-# function that reads the data
 def get_data(path):
+    """
+    Lê os dados de um arquivo CSV e retorna um DataFrame.
+
+    Parâmetros:
+    path (str): O caminho para o arquivo CSV que contém os dados.
+
+    Retorna:
+    pd.DataFrame: Um DataFrame contendo os dados do arquivo CSV.
+    """
+    # Lê os dados do arquivo CSV especificado no caminho 'path'
     df = pd.read_csv(path)
 
+    # Retorna o DataFrame contendo os dados lidos
     return df
--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -1,13 +1,24 @@
 import numpy as np
 
-# function that trata os valores 0
 def process_data(df):
+    """
+    Processa um DataFrame para tratar valores zero em colunas específicas e substituir por NaN,
+    seguido pelo preenchimento desses valores faltantes com a média da coluna.
 
-    # Column list to replace "0" values with NaN values
+    Parâmetros:
+    df (pd.DataFrame): O DataFrame contendo os dados a serem processados.
+
+    Retorna:
+    pd.DataFrame: O DataFrame processado com valores zero substituídos por NaN e valores faltantes
+                  preenchidos com a média da coluna.
+    """
+    # Lista de colunas para substituir valores "0" por valores NaN
     columns = ["Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI"]
-    # Replace 0 with np.nan in the specified columns
+
+    # Substitui 0 por np.nan nas colunas especificadas
     df[columns] = df[columns].replace(0, np.nan)
-    # Loop through each column and fill missing values with the mean of the column
+
+    # Itera por cada coluna e preenche valores faltantes com a média da coluna
     df.fillna({col: df[col].mean() for col in columns}, inplace=True)
 
     return df
