4af6b6b423c7ced161da1dc9d4745b5b69b1230d
Foiac <kaio_cfs@hotmail.com>
2024-07-27 22:24:18 -0300

    comments e doc string update

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,19 +4,39 @@ import mlflow
 import mlflow.sklearn
 
 def train_knn(x_train, x_test, y_train, y_test, n_neighbors):
+    """
+    Treina um modelo K-Nearest Neighbors (KNN) e registra o experimento no MLflow.
 
-    experiment_name = "mlflow-experiment-diabetes"
-    mlflow.set_experiment(experiment_name)
-    n_neighbors = n_neighbors #24
+    Parâmetros:
+    x_train (array-like): Dados de treino.
+    x_test (array-like): Dados de teste.
+    y_train (array-like): Rótulos de treino.
+    y_test (array-like): Rótulos de teste.
+    n_neighbors (int): Número de vizinhos a serem usados no algoritmo KNN.
+    """
     
+    # Nome do experimento MLflow
+    experiment_name = "mlflow-experiment-diabetes"
+    mlflow.set_experiment(experiment_name)  # Define o experimento no MLflow
+    
+    # Inicia uma nova execução de experimento no MLflow
     with mlflow.start_run():
         
+        # Registra automaticamente os parâmetros, métricas e modelos
         mlflow.sklearn.autolog()
-
-        knn = KNeighborsClassifier(n_neighbors = n_neighbors, metric = 'minkowski', p = 2)
+        
+        # Inicializa o modelo KNN com os parâmetros especificados
+        knn = KNeighborsClassifier(n_neighbors=n_neighbors, metric='minkowski', p=2)
+        
+        # Treina o modelo
         knn.fit(x_train, y_train)
-
-        Y_pred_knn=knn.predict(x_test)
-        score_knn = round(accuracy_score(y_test, Y_pred_knn)*100, 2)
-
+        
+        # Faz previsões no conjunto de teste
+        Y_pred_knn = knn.predict(x_test)
+        
+        # Calcula a acurácia do modelo
+        score_knn = round(accuracy_score(y_test, Y_pred_knn) * 100, 2)
+        print(f"Acurácia do modelo KNN: {score_knn}%")
+        
+        # Registra o modelo treinado no MLflow
         mlflow.sklearn.log_model(knn, "diabetesPredict")
